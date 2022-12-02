39e3152f20e7841f858187c7c3997988584b54be
soohyun jung <sasha981107@gmail.com>
2022-12-02 21:09:55 +0900

    Feat: 라우팅 & url 설정

--- my-app/src/routes/Router.jsx
+++ my-app/src/routes/Router.jsx
@@ -1,5 +1,36 @@
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom"
+import Login from "../pages/account/Login"
+import Register from "../pages/account/Register"
+import EditProfile from "../pages/account/EditProfile"
+import Splash from "../pages/splash/Splash"
+import HomeFeed from "../pages/feed/HomeFeed"
+import PostDetail from "../pages/feed/PostDetail"
+import UploadPost from "../pages/feed/UploadPost"
+import UploadProduct from "../pages/feed/UploadProduct"
+import ChatList from "../pages/chat/ChatList"
+import ChattingRoom from "../pages/chat/ChattingRoom"
+
 export default function Router() {
     return (
-        <div>Router</div>
+        <BrowserRouter basename="">
+            <Routes>
+            <Route path="/" element={<Splash />}/>
+            <Route path="/account/*" element={<Outlet />}>
+                <Route path="login/" element={<Login/>}/>
+                <Route path="register/" element={<Register/>}/>
+                <Route path="profile/" element={<EditProfile/>}/>
+            </Route>
+            <Route path="/post/*" element={<Outlet />}>
+                <Route path="" element={<HomeFeed/>}/>
+                <Route path=":id/" element={<PostDetail/>}/>
+                <Route path="upload/" element={<UploadPost/>}/>
+                <Route path="upload/product" element={<UploadProduct/>}/>
+            </Route>
+            <Route path="/chat/*" element={<Outlet />}>
+                <Route path="" element={<ChatList/>}/>
+                <Route path=":id/" element={<ChattingRoom/>}/>
+            </Route>
+            </Routes>
+        </BrowserRouter>
     )
 }
