91bb8696c575411315c73ec7336722e568b1fec9
SeongJun KIM <sjkymy@gmail.com>
2022-12-09 19:21:59 +0900

    Fix: props.type을 props.name으로 수정 #23

--- my-app/src/components/ButtonFollow.jsx
+++ my-app/src/components/ButtonFollow.jsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 const ButtonFollow = styled.button`
-    background-color: ${(props) => (props.type === 'follow'? '#F26E22' : '#fff')};
-    color: ${(props) => (props.type === 'follow'? '#fff' : '#767676')};
-    border: ${(props) => (props.type === 'follow'? 'none' : '1px solid #DBDBDB;')};
+    background-color: ${(props) => (props.name === 'follow'? '#F26E22' : '#fff')};
+    color: ${(props) => (props.name === 'follow'? '#fff' : '#767676')};
+    border: ${(props) => (props.name === 'follow'? 'none' : '1px solid #DBDBDB;')};
 
     &.ms {
         border-radius: 30px;
