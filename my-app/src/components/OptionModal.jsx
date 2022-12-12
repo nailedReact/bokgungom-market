70e9b39bb4f6091b901228761c292603e9621032
강세민 <ksm9802@gmail.com>
2022-12-12 18:25:33 +0900

    Feat: Option 모달 마크업 #42

--- my-app/src/components/OptionModal.jsx
+++
@@ -1,5 +0,0 @@
-export default function OptionModal() {
-    return (
-        <div>OptionModal</div>
-    )
-}
---
+++ my-app/src/components/shared/OptionModal/optionModal.style.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import modalIcon from "../../../assets/icon/icon-modal.png";
+
+export const OptionLayout = styled.article`
+    width: 390px;
+    padding: 16px 0 10px;
+    border-radius: 10px 10px 0 0;
+    background-color: white;
+
+    &::before {
+        content: "";
+        display: block;
+        width: 50px;
+        height: 4px;
+        margin: 0 auto 16px;
+        background-image: url(${modalIcon});
+    }
+
+    & .modal-items > * {
+        padding-top: 14px;
+        padding-bottom: 14px;
+        padding-left: 26px;
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 18px;
+    }
+`;
