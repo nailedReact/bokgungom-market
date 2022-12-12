4f9154eb4301a4df4e97fcdbaa12336ec4dec6a8
강세민 <ksm9802@gmail.com>
2022-12-12 15:51:51 +0900

    Feat: 사용자 인풋 경고 마크업 #32

--- my-app/src/components/shared/Warning/Warning.jsx
+++ my-app/src/components/shared/Warning/Warning.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { WarningItem } from "./warning.style";
 
 export default function Warning({ message, visible }) {
     if (visible) {
-        return <strong>*{message}</strong>;
+        return <WarningItem>*{message}</WarningItem>;
     }
 }
