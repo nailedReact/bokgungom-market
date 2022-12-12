589064ca89d824470587043b86ead053e2341252
강세민 <ksm9802@gmail.com>
2022-12-12 17:20:35 +0900

    Feat: 확인 모달창 마크업 #36

---
+++ my-app/src/components/shared/ConfirmModal/confirmModal.style.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const ConfirmlLayout = styled.article`
+    width: 252px;
+    border-radius: 10px;
+    background-color: white;
+
+    & h2 {
+        padding: 22px 0;
+        border-bottom: 0.5px solid #dbdbdb;
+        font-size: 16px;
+        line-height: 20px;
+        font-weight: 500;
+        text-align: center;
+    }
+
+    & .button-group > * {
+        display: inline-block;
+        width: 50%;
+        padding-top: 14px;
+        padding-bottom: 14px;
+        text-align: center;
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 18px;
+        background-color: transparent;
+    }
+
+    & .button-group :nth-child(2) {
+        border-left: 0.5px solid #dbdbdb;
+        color: #F26E22;
+    }
+`;
