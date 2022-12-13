51df48505c83064f77eb8769bacb63812d3d7d88
Soohyun Jung <sasha1107@naver.com>
2022-12-13 14:27:21 +0900

    Feat: 로그인 유효성 검사 #47
    - 로그인 유효성 검사 후 에러메시지 디스플레이
    - 로그인 성공 시 로컬 스토리지에 토큰 저장

--- my-app/src/pages/account/Login.jsx
+++ my-app/src/pages/account/Login.jsx
@@ -1,79 +1,105 @@
-import React from 'react'
-import {useState, useEffect} from 'react'
+import { useState, useEffect, useRef } from 'react'
+import Inp from '../../components/userinput/Inp';
+import UserInput from '../../components/userinput/UserInput';
+import Button from '../../components/Button';
+import Warning from '../../components/Warning';
 
 export default function Login() {
-  const [email, setEmail] = useState('')
-  const [pw, setPw] = useState('')
-  const [button, setButton] = useState(true);
+    const emailInp = useRef(null);
+    const pwInp = useRef(null);
+    const [emailVal, setEmailVal] = useState("");
+    const [pwVal, setPwVal] = useState("");
+    const [isBtnDisable, setIsBtnDisable] = useState(true);
+    const [emailValid, setEmailValid] = useState(false);
+    const [resStat, setResStat] = useState();
+    const AlertMsg = useRef(null);
 
-  const handleInputId = (e) => {
-    setEmail(e.target.value)
-  }
-  const handleInputPw = (e) => {
-    setPw(e.target.value)
-  }
+    // 이메일 형식이 유효하고 &&  비밀번호가 6자리 이상이면 버튼 활성화
+    useEffect(() => {
+        emailValid && pwVal.length >= 6 ? setIsBtnDisable(false) : setIsBtnDisable(true);
+    }, [emailValid, pwVal])
 
-  // 이메일에 @가 포함되고 비밀번호가 6자리 이상이면 버튼 활성화
-  useEffect(() => {
-    email.includes('@') && pw.length >= 6 ? setButton(false) : setButton(true);
-  }, [email, pw])
-
-  // 로그인 버튼 클릭하면 api전송
-  const onClickLogin = (e) => {
-    const fetchData = async() => {
-      const url = "https://mandarin.api.weniv.co.kr/user/login";
-      const loginData = {
-        "user":{
-          "email": email,
-          "password": pw
+    // 이메일 타입 유효성 검사 
+    const handleValidation = (e) => {
+        if (e.target.id === "email"){
+            setEmailVal(emailInp.current.value)
+            !e.currentTarget.validity.typeMismatch ? setEmailValid(true) : setEmailValid(false);
         }
-      };
-      const response = await fetch(url,{
-        method:"POST",
-        headers:{
-            "Content-type" : "application/json"
-        },
-        body:JSON.stringify(loginData)
-      });
-      const json = await response.json();
-      console.log(json);
+        else if (e.target.id === "pw"){
+            setPwVal(pwInp.current.value);
+        }   
+    }
 
-      // 로그인이 성공하면 로컬스토리지에 토큰 저장
-      const token = json.user["token"]
-      localStorage.setItem("Authorization","Bearer " + token);
-    };
-    fetchData()
-  }
-  
-  return (
-    <main className="container">
-        <h1 className="tit_login">로그인</h1>
-        <form>
-            <label htmlFor="inpEmail">이메일</label>
-            <input
-              type="email"
-              name="inpEmail"
-              className="login"
-              value={email}
-              onChange={handleInputId}
-            />
-
-            <label htmlFor="inpPw">비밀번호</label>
-            <input
-            type="password"
-            name="inpPw"
-            className="login"
-            value={pw}
-            onChange={handleInputPw}
-            />
-
-            <button type='button' className="btn_login" disabled={button}
-            onClick={onClickLogin} >
-              로그인
-            </button>
-
-            <button className="btn_join">이메일로 회원가입</button>
-        </form>
-    </main>
-  )
+    // 로그인 버튼 클릭하면 api전송
+    const handleLogin = (e) => {
+        const fetchData = async() => {
+            const url = "https://mandarin.api.weniv.co.kr/user/login";
+            const loginData = {
+                "user":{
+                "email": emailVal,
+                "password": pwVal
+                }
+            };
+            const response = await fetch(url,{
+                method:"POST",
+                headers:{
+                    "Content-type" : "application/json"
+                },
+                body:JSON.stringify(loginData)
+            });
+            const res = await response.json();
+            // console.log(res);
+            if (res.message){
+                console.log('로그인 실패');
+                AlertMsg.current.textContent = "*" + res.message;
+                AlertMsg.current.style.display = "block";
+            }
+            else if (res.user){
+                console.log('로그인 성공');
+                // 로그인이 성공하면 로컬스토리지에 토큰 저장
+                const token = res.user["token"];
+                localStorage.setItem("Authorization","Bearer " + token);
+                AlertMsg.current.style.display = "none";
+            }
+        }
+        fetchData();
+    }
+    
+    return (
+        <main className="container">
+            <h1 className="tit_login">로그인</h1>
+            <form>
+                <UserInput inputId="email" label="이메일">
+                    <Inp
+                        type="email"
+                        id="email"
+                        ref={emailInp}
+                        onChange={handleValidation}
+                        required
+                    >
+                    </Inp>
+                </UserInput>
+                <UserInput inputId="pw" label="비밀번호">
+                    <Inp
+                        type="password"
+                        id="pw"
+                        ref={pwInp}
+                        onChange={handleValidation}
+                        required
+                    >
+                    </Inp>
+                </UserInput>
+                <Warning ref={AlertMsg}>
+                    로그인 오류 메시지
+                </Warning>
+                <Button
+                    type="button"
+                    className="large"
+                    disabled={isBtnDisable}
+                    onClick={handleLogin}
+                >다음</Button>
+                <button className="btn_join">이메일로 회원가입</button>
+            </form>
+        </main>
+    )
 }
