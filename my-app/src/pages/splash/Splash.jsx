9908a3c15ae1999db22a533d8cb8e8736d369deb
hyejihan <108308915+hyejee0504@users.noreply.github.com>
2022-12-15 10:04:27 +0900

    feat: 판매중인 상품 기능 구현, 마크업, 스타일링

--- my-app/src/pages/splash/Splash.jsx
+++ my-app/src/pages/splash/Splash.jsx
@@ -1,10 +1,11 @@
-import useAuth from "../../hook/useAuth"
+// import useAuth from "../../hook/useAuth"
+import Profile from "../profile/Profile"
 
 export default function Splash() {
     // 로그인 정보 받아오기
-    const data = useAuth();
-    console.log(data)
+    // const data = useAuth();
+    // console.log(data)
     return (
-        <div>splash</div>
+        <Profile />
     )
 }
