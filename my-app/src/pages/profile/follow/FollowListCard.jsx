8e02a5a58e0220950535aaf28f34e405a9f13d3e
Soohyun Jung <sasha1107@naver.com>
2022-12-20 16:33:08 +0900

    Feat: 팔로우 & 팔로잉 목록에서 프로필 클릭 시 해당 사용자 프로필로 이동 #148

--- my-app/src/pages/profile/follow/FollowListCard.jsx
+++ my-app/src/pages/profile/follow/FollowListCard.jsx
@@ -3,10 +3,9 @@ import styled from 'styled-components';
 import Button from '../../../components/Button';
 import axios from 'axios';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
 
-//취소버튼을 누르면 팔로우 취소가 된 데이터가 넘어가야합니다! - 구현예정
-export default function FollowListCard({data}) {
-    const Followingimg = styled.img`
+const Followingimg = styled.img`
         height: 50px;
         width: 50px;
         border-radius: 0px;
@@ -41,8 +40,16 @@ export default function FollowListCard({data}) {
         align-items: center;
     `;
 
+//취소버튼을 누르면 팔로우 취소가 된 데이터가 넘어가야합니다! - 구현예정
+export default function FollowListCard({data}) {
+    const navigate = useNavigate();
     const [checkFollowing, setCheckFollowing] = useState(data.isfollow)
-    
+
+    // 팔로우한 사람의 프로필 클릭 시 그 사람의 프로필로 이동
+    function handleClickProfile(){
+        navigate("../../../account/profile/" + data.accountname);
+    }
+
       const followingchange = async(e) => {
         console.log(data); 
         if(e.target.value === "true"){
@@ -60,7 +67,6 @@ export default function FollowListCard({data}) {
             const follow = await axios.post(
                 `https://mandarin.api.weniv.co.kr/profile/${data.accountname}/follow`,{},{
                 headers: {
-                    // Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzOTY5MWQwMTdhZTY2NjU4MWMzMjM1YyIsImV4cCI6MTY3NTk5NjE5MywiaWF0IjoxNjcwODEyMTkzfQ.yX_F68SQOJkak0ud8BUTI3OUHriaIlPqEqDUiWBcf6I"
                   Authorization: localStorage.getItem("Authorization")
                     }
                 });
@@ -70,8 +76,8 @@ export default function FollowListCard({data}) {
 
   return (
     <Cont>
-        <Followingimg src={data.image} alt="팔로잉한 사람 프로필사진" />
-        <TxtCont>
+        <Followingimg src={data.image} alt="팔로잉한 사람 프로필사진" onClick={handleClickProfile}/>
+        <TxtCont onClick={handleClickProfile}>
             <Username>{data.username}</Username>
             <Intro>{data.intro}</Intro>
         </TxtCont>
