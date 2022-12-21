e91f399cf73db5150e8190c4de5cd8b5a2a1e45b
강세민 <ksm9802@gmail.com>
2022-12-21 13:01:25 +0900

    Style: optionModal CSS style #162
    - max-width: 390px
    - 모달 내 버튼이 width 100%가 됨으로써 편의성 증대
    - 페이지 내 수평 정렬

--- my-app/src/components/OptionModal/optionModal.style.js
+++ my-app/src/components/OptionModal/optionModal.style.js
@@ -2,10 +2,13 @@ import styled from "styled-components";
 import modalIcon from "../../assets/icon/icon-modal.png";
 
 export const OptionLayout = styled.article`
+    max-width: 390px;
     width: 100%;
     /* position: absolute; */
     position: fixed;
     bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
     padding: 16px 0 10px;
     border-radius: 10px 10px 0 0;
     z-index: 100;
@@ -26,6 +29,15 @@ export const OptionLayout = styled.article`
         padding-left: 26px;
         font-size: 14px;
         font-weight: 400;
+        line-height: 18px;
+    }
+
+    & .modal-items button {
+        display: block;
+        width: 100%;
+        text-align: left;
+        background-color: transparent;
+        font-size: 14px;
         line-height: 18px;
     }
 `;
