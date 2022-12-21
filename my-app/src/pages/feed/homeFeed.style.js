a4d21f35b046d29e4bd5f2e84747e618c88e0afd
Soohyun Jung <sasha1107@naver.com>
2022-12-21 16:10:25 +0900

    Style: 홈 피드 반응형 구현 #174
    - 가로 사이즈 768px 이상일 때 내비바 위치 변경

--- my-app/src/pages/feed/homeFeed.style.js
+++ my-app/src/pages/feed/homeFeed.style.js
@@ -1,5 +1,15 @@
 import styled from "styled-components";
 
 export const FeedCont = styled.div`
-    padding-bottom: 60px;
+    display: flex;
+    flex-direction: column;
+    /* justify-content: center; */
+    /* align-items: center; */
+
+    @media screen and (max-width: 768px){
+        padding-bottom: 60px;
+    }
+    @media screen and (min-width: 768px){
+        margin-left: 240px;
+    }
 `
