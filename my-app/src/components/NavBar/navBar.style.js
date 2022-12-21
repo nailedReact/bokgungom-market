1156087b870fb66538dc013896c93d90939888a2
Soohyun Jung <sasha1107@naver.com>
2022-12-21 15:11:33 +0900

    Style: 내비바 반응형 레이아웃 구현 #174

--- my-app/src/components/NavBar/navBar.style.js
+++ my-app/src/components/NavBar/navBar.style.js
@@ -7,8 +7,22 @@ export const NavBarCont = styled.div`
     position: fixed;
     bottom : 0;
     background-color: white;
+    
+    @media screen and (min-width: 768px){
+        left : 0;
+        width: 30%;
+        border-top: none;
+        border: 0.5px solid #DBDBDB;
+        height: 100%;
+        padding-top: 50px;
+    }
 `;
 export const NavBarUl = styled.ul`
     display: flex;
     justify-content: space-around;
+    @media screen and (min-width: 768px){
+        flex-direction: column;
+        gap: 20px;
+        padding: 20px 0px;
+    }
 `
