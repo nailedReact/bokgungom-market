5087a85919dc068014db1f55384bdaf967c75385
hyejihan <108308915+hyejee0504@users.noreply.github.com>
2022-12-26 23:39:55 +0900

    feat: 페이지네이션

--- my-app/src/pages/feed/homeFeed.style.js
+++ my-app/src/pages/feed/homeFeed.style.js
@@ -15,3 +15,21 @@ export const FeedCont = styled.div`
         margin-left: 240px;
     }
 `
+export const PageBtn = styled.button`
+    font-family: "Pretenard";
+    padding: 8px 12px;
+    border: 1px solid var(--color-primary);
+    border-radius: 50%;
+    background: white;
+`
+export const PrevNextBtn = styled.button`
+    font-family: "Pretenard";
+    padding: 8px;
+    border-radius: 50%;
+    background: var(--color-primary);
+    color: white;
+
+    &:disabled {
+        opacity: 0.4;
+    }
+`
