42abb0cd5d986b81a7489237d3c987b04bdd85f0
Soohyun Jung <sasha1107@naver.com>
2023-01-09 21:07:48 +0900

    Feat: 스프라이트 아이콘 사용 기반 마련 #376
    - 전역에서 로드
    - SVGIcon 컴포넌트를 사용하여 불러와 사용 가능

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,11 @@
 import Router from "./routes/Router";
 import { GlobalStyle } from "./app.style";
+import { spriteSVGIcon } from "./components/icon/spriteIcon";
 
 function App() {
     return (
         <>
+            {spriteSVGIcon}
             <GlobalStyle />
             <Router />
         </>
