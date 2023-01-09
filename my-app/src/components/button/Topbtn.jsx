620be876df6c0220ef079eb50cd449a6b07eac02
Soohyun Jung <sasha1107@naver.com>
2023-01-09 22:25:08 +0900

    Refactor: 탑버튼 스타일 분리 #376

--- my-app/src/components/button/Topbtn.jsx
+++ my-app/src/components/button/Topbtn.jsx
@@ -1,32 +1,8 @@
-import styled from 'styled-components';
 import SVGIcon from '../icon/SVGIcon';
-
-const TopbtnCont = styled.div`
-  position: fixed;
-  right: 3%;
-  bottom: 4%;
-  z-index: 1;
-  @media screen and (max-width: 768px){
-    margin-bottom: 60px;
-  }
-
-  & p {
-    width: 0;
-    height: 0;
-    font-size: 0;
-  }
-`
-
-const Topbutton = styled.button`
-  padding :8px 10px;
-  background-color: #4584a3;
-  border-radius: 50%;
-  box-shadow: rgb(0 0 0 / 25%) 0px 3px 10px;
-  &:hover{
-    background-color: #78cafe;;
-  }
-
-`
+import {
+  TopbtnCont,
+  Topbutton
+} from "./topbtn.style";
 
 export default function Topbtn() {
     
