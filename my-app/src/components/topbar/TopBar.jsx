cae921390bd361c9fe355cfbb3e8ae6716f16141
Soohyun Jung <sasha1107@naver.com>
2023-01-09 21:38:51 +0900

    Refactor: 상단바 스타일 파일 분리 #376

--- my-app/src/components/topbar/TopBar.jsx
+++ my-app/src/components/topbar/TopBar.jsx
@@ -1,93 +1,16 @@
-import styled, { css } from "styled-components"
 import Button from "../button/Button"
 import { useNavigate } from 'react-router-dom';
-import Logo from "../../assets/logo.png"
 import SVGIcon from "../icon/SVGIcon"
+import { 
+    TopBarCont,
+    LeftCont,
+    RightCont,
+    BtnIcon,
+    Searchinput,
+    LogoCont
 
-const TopBarCont = styled.div`
-    position: sticky;
-    top: 0;
-    width: 100%;
-    height: 55px;
-    z-index: 10;
-    background-color: white;
-    border-bottom: 0.5px solid #DBDBDB;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 12px 12px 12px 16px;
-    border-bottom: 0.5px solid #DBDBDB;
-    transition: .3s;
-    @media screen and (min-width: 768px){
-        background-color: #C6D9E3;
-        height: 60px;
-    }
-`
-const LeftCont = styled.div`
-    display: flex;
-    gap: 10px;
-    align-items: center;
+} from "./topbar.style"
 
-`
-const RightCont = styled.div`
-    display: flex;
-    width: 50%;
-    justify-content: right;
-`
-
-const BtnIcon = styled.button`
-    ${({action}) => action === "back" && css`
-        & svg {
-            left: 14px;
-        }
-    `};
-    ${({action}) => (action === "more" ||  action === "search") && css`
-        & svg {
-            right: 14px;
-        }
-    `};
-    background: none;
-    display: block;
-    width: 24px;
-    height: 24px;
-    & svg {
-        fill: none;
-        position: absolute;
-        bottom: 14px;
-        @media screen and (min-width: 768px){
-            bottom: 18px;
-        }
-    }
-`;
-
-const Searchinput = styled.input`
-    font-size: 14px;
-    background: #F2F2F2;
-    border-radius: 30px;
-    border: none;
-    padding: 8px;
-    transition: .5s;
-    width: inherit;
-    width: 80%;
-    &:focus {
-        width: 100%;
-    }
-    @media screen and (max-width: 768px){
-        width: 90%;
-    }
-`;
-
-const LogoCont = styled.div`
-background: url(${Logo});
-background-size: cover;
-width: 162px;
-height: 40px;
-position: absolute;
-@media screen and (max-width: 768px){
-    display: none;
-}
-cursor: pointer;
-`
 
 export default function TopBar({type, title, right4Ctrl, onChangeByUpper, onClickGetMsg, onClickModal}) {
     // type의 앞글자, type의 뒤의 글자를 변수에 저장한다.
