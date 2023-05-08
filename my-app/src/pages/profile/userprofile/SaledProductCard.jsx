076504dbb85947e7786035d47795bff215a21db2
hyejihan <108308915+hyejee0504@users.noreply.github.com>
2023-05-08 15:23:40 +0900

    Bug : 판매중인 상품 가져오는 부분 헤더 변경 (오류 해결)

--- my-app/src/pages/profile/userprofile/SaledProductCard.jsx
+++ my-app/src/pages/profile/userprofile/SaledProductCard.jsx
@@ -29,7 +29,7 @@ useEffect(() => {
         const URL = `${baseUrl}/product/` + username;
       const res = await axios.get(URL, {
         headers: {
-          Authorization : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzOTY5MWQwMTdhZTY2NjU4MWMzMjM1YyIsImV4cCI6MTY3NTk5NjE5MywiaWF0IjoxNjcwODEyMTkzfQ.yX_F68SQOJkak0ud8BUTI3OUHriaIlPqEqDUiWBcf6I"
+          Authorization : localStorage.getItem("Authorization")
         }
     });
         setResMsg(res.data.product);
