f445000c63fdcad4e99251334b90683baaf22fd8
SeminKang <ksm9802@gmail.com>
2023-06-01 01:59:01 +0900

    Fix: 개별 채팅방 데스크 스타일 문제 #405
    * 문제 상황: 데스크탑 버전에서 개별 채팅방 왼쪽에 여백이 생김
    * 이유: 채팅리스트 페이지와 개별 채팅방에서 공통적인 스타일드 컴포넌트 사용하기 때문
    * 해결: 각 페이지에서 사용하는 스타일드 컴포넌트 구분

--- my-app/src/pages/chat/chatListRoom.style.js
+++ my-app/src/pages/chat/chatListRoom.style.js
@@ -11,10 +11,6 @@ const ChatContBack = styled.div`
 const ChatCont = styled.ul`
     overflow-y: scroll;
     padding: 20px 16px 16px 16px;
-    @media screen and (min-width: 768px) {
-        margin-left: 240px;
-        padding: 40px;
-    }
 `;
 
 export {ChatContBack, ChatCont};
